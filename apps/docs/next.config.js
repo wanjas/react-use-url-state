eade8626d2c91269259354d6ef3b05fd58c5a4e3
Ivan Belikov <wanjas@gmail.com>
2023-11-30 11:52:38 +0900

    feat: GA

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -10,6 +10,9 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  experimental: {
+    nextScriptWorkers: true,
+  },
 };
 
 const withNextra = require('nextra')({
