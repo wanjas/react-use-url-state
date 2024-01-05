cf70d15814c6e0c00ed7ec7b0d0f4d88aa030c44
Ivan Belikov <wanjas@gmail.com>
2024-01-05 19:32:52 +0900

    docs: new domain in sitemap

--- apps/docs/next-sitemap.config.js
+++ apps/docs/next-sitemap.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://react-use-url-state.vercel.app',
+  siteUrl: 'https://react-use-url-state.wrigglework.com',
   generateRobotsTxt: true,
   sourceDir: path.resolve(__dirname, '../../dist/apps/docs/.next'),
   generateIndexSitemap: false,
